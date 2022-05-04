67673acbeea131f905bb5f6646e65e977e0df227
Snehal Tilekar <snehaltilekar18@gmail.com>
2022-05-04 18:13:59 +0530

    Added hello program

---
+++ gitdata/Hello.java
@@ -0,0 +1,16 @@
+package SampleJavaProgram;
+import java.util.*;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter message to display on console:");
+		String msg = sc.next();
+		System.out.println(msg);
+		System.out.println("Welcome to Software Testing...!!!!");
+		
+
+	}
+
+}
